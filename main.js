ad272e2b99e08a238fd1ad73fabf324b7b4df5af
theItalianDev <italiandev@protonmail.com>
2022-11-04 22:09:45 +0100

    final code

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+const form = document.querySelector("form");
+const tipModal = document.getElementById("tip-modal")
+
+if (!window.ethereum) {
+    tipModal.classList.add("no-wallet")
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event)
+});
